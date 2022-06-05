99004a1bb5ed27322e717780d2fd72c4dfd1c1df
Igor Polishchuk <amadare42@gmail.com>
2022-06-05 05:53:08 +0300

    added changes to reproduce stack overflow issue on JSON parsing

--- src/main.rs
+++ src/main.rs
@@ -12,17 +12,19 @@ mod lambda;
 
 const STACK_SIZE: usize = 4 * 1024 * 1024;
 
-fn main() {
+#[tokio::main]
+async fn main() {
     configure_logging();
+    telegram::run_polling().await.unwrap();
 
     // running actual logic in different thread so stack size could be set
     // otherwise frankenstein can fail with STACK_OVERFLOW on larger json responses
-    std::thread::Builder::new()
-        .stack_size(STACK_SIZE)
-        .spawn(run)
-        .unwrap()
-        .join()
-        .unwrap();
+    // std::thread::Builder::new()
+    //     .stack_size(STACK_SIZE)
+    //     .spawn(run)
+    //     .unwrap()
+    //     .join()
+    //     .unwrap();
 }
 
 fn run() {
